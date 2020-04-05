10c7df8b3a053870e66647a3936c422926b8421e
Martinkovics Krisztián <martinkovics.krisztian@gmail.com>
2020-04-05 18:59:56 +0200

    add okhttp

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    var client = OkHttpClient()
+
+    fun run() {
+        val request = Request.Builder()
+            .url("https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY")
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            for ((name, value) in response.headers) {
+                println("$name: $value")
+            }
+
+            println(response.body!!.string())
+        }
     }
 }
